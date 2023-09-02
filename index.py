ad43f8e616cd9a2eb528e2890be83d4751881e6e
dominiek1974 <61291459+dominiek1974@users.noreply.github.com>
2023-09-02 11:43:48 +0200

    regel 8 een spacy

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ print("hallo")
 for i in range(50, 100, +3):
     print(i)
 
-A = input("welk getal vermenigvuldigen met 5?")
+A = input("welk getal vermenigvuldigen met 5? ")
 B = float(A)
 C = B*5
 print(C)
